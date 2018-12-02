a3d1bc64a51d3a882802fb061b6faec2a340ad84
v3nd3tt4 <okta.pilopa@staff.itera.ac.id>
2018-12-02 10:21:01 +0700

    script backup db

--- application/controllers/Backup.php
+++ application/controllers/Backup.php
@@ -55,6 +55,6 @@ class Backup extends CI_Controller {
 		$dir = $ket.'.sql';
 		echo "<h3>Backing up database to `<code>{$dir}</code>`</h3>";
 		exec("/Applications/XAMPP/bin/mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
-		var_dump($output);
+		//var_dump($output);
     }
 }
