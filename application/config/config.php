47d95809b48132ec6cf8bba27632968ad74d7e62
v3nd3tt4 <pilopaokta@gmail.com>
2019-06-27 16:56:59 +0700

    from mac

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 
-$config['base_url'] = 'http://localhost/spp/';
+$config['base_url'] = 'http://localhost/sppvendetta/';
 // $config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/';
 
 /*
@@ -383,7 +383,8 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+// $config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
